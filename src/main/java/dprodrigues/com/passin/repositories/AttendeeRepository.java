10b05d5ff79da42cd9ee918b0c6ec69ae83ed8d1
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-04-04 07:58:56 -0300

    Initial commit

---
+++ src/main/java/dprodrigues/com/passin/repositories/AttendeeRepository.java
@@ -0,0 +1,7 @@
+package dprodrigues.com.passin.repositories;
+
+import dprodrigues.com.passin.domain.attendee.Attendee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+}
---
+++ src/main/java/dprodrigues/com/passin/repositories/CheckinRepository.java
@@ -0,0 +1,7 @@
+package dprodrigues.com.passin.repositories;
+
+import dprodrigues.com.passin.domain.checkin.CheckIn;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckinRepository extends JpaRepository<CheckIn, Integer> {
+}
